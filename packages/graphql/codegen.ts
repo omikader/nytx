b5e40e76b5207a8ad9e825c97c26d785aa54a031
Omar Abdelkader <omikader@gmail.com>
2023-05-16 07:47:23 -0400

    Fix graphql version and some formatting

--- packages/graphql/codegen.ts
+++ packages/graphql/codegen.ts
@@ -4,10 +4,10 @@ const config: CodegenConfig = {
   schema: "schema.graphql",
   generates: {
     "../functions/api/src/resolvers-types.ts": {
+      plugins: ["typescript", "typescript-resolvers"],
       config: {
         contextType: "./index#IContext",
       },
-      plugins: ["typescript", "typescript-resolvers"],
     },
   },
 };
